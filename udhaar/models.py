3390006a4738a1492abb3255b2e208f800e57932
Siddhant Chhabra <siddhantchhabra96@gmail.com>
2017-10-03 23:57:17 +0530

    Created Udhaar Models

--- udhaar/models.py
+++ udhaar/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Groups(models.Model):
+    gid = models.IntegerField()
+    uname = models.CharField(max_length=200)
+
+class Transactions(models.Model):
+    gid = models.ForeignKey(Groups, on_delete=models.CASCADE)
+    to  = models.CharField(max_length=200)
+    from_user = models.CharField('from',max_length=200)
+    amount = models.IntegerField()
